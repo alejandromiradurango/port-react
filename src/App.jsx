c137a97bef0c1f793a2d638b6cfec95a552ba26c
alejandro.mira <alejandro.mira@optimus3d.com>
2022-08-29 00:43:04 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Navbar, Footer, Hero, Sidebar} from './components/index'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export {default as Navbar } from "./Navbar";
+export {default as Sidebar } from "./Sidebar";
+export {default as Hero } from "./Hero";
+export {default as AboutMe } from "./AboutMe";
+export {default as Techs } from "./Techs";
+export {default as Projects } from "./Projects";
+export {default as Contact } from "./Contact";
+export {default as Footer } from "./Footer";
